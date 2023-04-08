fff147ae5382ef5b592bfafd792a7f557470a8c9
SameerJadav <sameerjadav010@gmail.com>
2023-04-08 20:33:35 +0530

    Edge config

--- next.config.js
+++ next.config.js
@@ -1,9 +1,17 @@
-const { withContentlayer } = require('next-contentlayer');
+const { get } = require("@vercel/edge-config");
+const { withContentlayer } = require("next-contentlayer");
 
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   experimental: {
     appDir: true,
+  },
+  redirects() {
+    try {
+      return get("redirects");
+    } catch {
+      return [];
+    }
   },
 };
 
