9972e5d4d508e1295ba7137aef6b4807bd30e6ce
SameerJadav <sameerjadav010@gmail.com>
2023-04-08 20:43:15 +0530

    Removed redirects and header

--- next.config.js
+++ next.config.js
@@ -1,25 +1,9 @@
-const { get } = require("@vercel/edge-config");
 const { withContentlayer } = require("next-contentlayer");
 
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   experimental: {
     appDir: true,
-  },
-  redirects() {
-    try {
-      return get("redirects");
-    } catch {
-      return [];
-    }
-  },
-  headers() {
-    return [
-      {
-        source: "/(.*)",
-        headers: securityHeaders,
-      },
-    ];
   },
 };
 
